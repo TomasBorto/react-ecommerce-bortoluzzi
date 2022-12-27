dbe2f3c224dcd860e7b75766fca7f20fb2111c19
Borto <bptomy1@gmail.com>
2022-12-27 17:58:35 -0300

    primer commit

---
+++ src/components/CartWidget.js
@@ -0,0 +1,10 @@
+const CartWidget = () => {
+    return (
+        <div style={{backgroundColor: 'white', color: 'black', padding: '10px'}}>
+            <img style={{ height: '35px', width: '50px'}} src='./carrito.png' alt="cartwidget"/>
+            0
+        </div>
+    )
+}
+
+export default CartWidget
